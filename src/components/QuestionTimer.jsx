86b13c100d6ec0e890d98e968ff8af2e1853970d
BinderK <kevinbinder@gmx.at>
2025-11-20 10:03:48 -0500

    Initial commit

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+export default function QuestionTimer({ time, onTimeout, mode }) {
+  const [remainingTime, setRemainingTime] = useState(time);
+
+  useEffect(() => {
+    const timer = setTimeout(onTimeout, time);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [time, onTimeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 100);
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <progress
+      id="question-time"
+      className={mode}
+      max={time}
+      value={remainingTime}
+    />
+  );
+}
